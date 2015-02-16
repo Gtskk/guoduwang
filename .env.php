82911d2e34175cc4af2eb25f98d25833f5e94dd2
gtskk <tttt6399998@126.com>
2015-02-16 13:18:34 +0800

    new file .env.php

---
+++ .env.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+
+        'DATABASE_USER' => 'knBHESLgKIpqYRMg',
+        'DATABASE_PASS' => 'bjm4THvmGch1cXbq',
+        'url' => 'http://guoduwang-a7268.codingapp.com',
+        'client_id' => 'f5055a8508d5387e7a49',
+        'client_secret' => '51b0535be6dcd04f8ae962d88820476f9c76eca9'
+);
+    
