13e3b21120b4db889bcb27760ec56574e9d44af4
Rock Zhang <iamgtskk@gmail.com>
2015-04-20 15:52:22 +0800

    更新网站url

--- app/config/app.php
+++ app/config/app.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'url' => 'http://localhost/guoduwang/public',
+	'url' => 'http://guoduwang.net/public',
 
 	/*
 	|--------------------------------------------------------------------------
